66509b9063d4345c8e31492ee4e49cc4a8854659
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 01:47:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ end
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    
+    holiday.each do |holiday, supplies|
     
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
