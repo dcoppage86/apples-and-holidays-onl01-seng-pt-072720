be213046ad1679f02eb080f5dfb48ef7ad9b43f8
Dakota Coppage <dakotakcoppage@me.com>
2020-08-10 01:55:49 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,6 +66,12 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.map do |season, holiday|
+    holiday.map do |holiday, supply|
+      holiday if supply.include?("BBQ")
+    end
+  end.flatten.compact
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
